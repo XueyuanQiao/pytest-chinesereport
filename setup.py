d089364712164a14f366b09cbee1a5360d9e4c83
JoeQiao <joe.qiao@uptech.ai>
2025-03-19 18:09:56 +0800

    1.0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='pytest-chinesereport',
-    version='1.0.2',
+    version='1.0.3',
     author='qiao',
     author_email='1336582921@qq.com',
     maintainer='qiao',
